7b5d57af6af1954718108d1909abb262ed5aae6d
juliano nassar <juliano nassar>
2017-05-19 09:59:19 -0300

    add

--- Voz.py
+++ Voz.py
@@ -1,11 +1,12 @@
 from gtts import gTTS
-import os,pygame,time
+import os,pygame
 
 def Falagg(fala):
-    tts = gTTS(text=fala, lang='pt')
-    tts.save("good.mp3")
-    pygame.mixer.init()
-    pygame.mixer.music.load("good.mp3")
-    pygame.mixer.music.play() 
-    pygame.mixer.quit()
-    os.remove("good.mp3")
+    #tts = gTTS(text=fala, lang='pt')
+    #ts.save("Fala.mp3")
+    #pygame.mixer.init()
+    #pygame.mixer.music.load("Fala.mp3")
+    #pygame.mixer.music.play() 
+    #pygame.mixer.quit()
+    #os.remove("Fala.mp3")
+    print(fala)
